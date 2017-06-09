82da0db8a27d67279e040b35a91c5d9d4985bf7a
Naveen New <User-2@gmail.com>
2017-06-09 16:34:02 +0530

    SQL Added

---
+++ MySQL.sql
@@ -0,0 +1,97 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.7.12-log : Database - liferaynew_1
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `wiki_comment_table` */
+
+DROP TABLE IF EXISTS `wiki_comment_table`;
+
+CREATE TABLE `wiki_comment_table` (
+  `comment_ID` bigint(20) NOT NULL,
+  `Comment_data` text,
+  `Comment_User` varchar(500) DEFAULT NULL,
+  `Comment_Date` varchar(1000) DEFAULT NULL,
+  `PageID` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`comment_ID`),
+  KEY `FK_wiki_comment_table` (`PageID`),
+  CONSTRAINT `FK_wiki_comment_table` FOREIGN KEY (`PageID`) REFERENCES `wiki_page_table` (`PageID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `wiki_comment_table` */
+
+insert  into `wiki_comment_table`(`comment_ID`,`Comment_data`,`Comment_User`,`Comment_Date`,`PageID`) values (1,'This is me','Test','6/9/2017',1);
+
+/*Table structure for table `wiki_node_table` */
+
+DROP TABLE IF EXISTS `wiki_node_table`;
+
+CREATE TABLE `wiki_node_table` (
+  `NodeID` bigint(20) NOT NULL,
+  `NodeName` varchar(500) DEFAULT NULL,
+  `NodeDescription` varchar(1000) DEFAULT NULL,
+  `NodeCreationDate` varchar(200) DEFAULT NULL,
+  `NodeCreator` varchar(200) DEFAULT NULL,
+  `NodeModificationDate` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`NodeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `wiki_node_table` */
+
+insert  into `wiki_node_table`(`NodeID`,`NodeName`,`NodeDescription`,`NodeCreationDate`,`NodeCreator`,`NodeModificationDate`) values (1,'Main','This is my First','6/9/2017','Test','6/9/2017'),(2,'Second',NULL,NULL,NULL,NULL);
+
+/*Table structure for table `wiki_page_table` */
+
+DROP TABLE IF EXISTS `wiki_page_table`;
+
+CREATE TABLE `wiki_page_table` (
+  `PageID` bigint(20) NOT NULL,
+  `PageName` varchar(500) DEFAULT NULL,
+  `PageDescription` varchar(1000) DEFAULT NULL,
+  `PageRating` varchar(100) DEFAULT NULL,
+  `PageVersion` varchar(100) DEFAULT NULL,
+  `NodeID` bigint(20) DEFAULT NULL,
+  `Page_Creation_Date` varchar(500) DEFAULT NULL,
+  `Page_creator` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`PageID`),
+  KEY `FK_wiki_page_table` (`NodeID`),
+  CONSTRAINT `FK_wiki_page_table` FOREIGN KEY (`NodeID`) REFERENCES `wiki_node_table` (`NodeID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `wiki_page_table` */
+
+insert  into `wiki_page_table`(`PageID`,`PageName`,`PageDescription`,`PageRating`,`PageVersion`,`NodeID`,`Page_Creation_Date`,`Page_creator`) values (1,'ONE','This is One Page','5','2',1,'6/9/2017','TEST'),(2,'TWO','This is Two Page','5','1',1,'6/9/2017','TEST');
+
+/*Table structure for table `wiki_pagedata_table` */
+
+DROP TABLE IF EXISTS `wiki_pagedata_table`;
+
+CREATE TABLE `wiki_pagedata_table` (
+  `Page_ModID` bigint(20) NOT NULL,
+  `PageData` text,
+  `PageEdit_User` varchar(500) DEFAULT NULL,
+  `Page_editDate` varchar(500) DEFAULT NULL,
+  `Page_Version` varchar(500) DEFAULT NULL,
+  `PageID` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`Page_ModID`),
+  KEY `FK_wiki_pagedata_table` (`PageID`),
+  CONSTRAINT `FK_wiki_pagedata_table` FOREIGN KEY (`PageID`) REFERENCES `wiki_page_table` (`PageID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `wiki_pagedata_table` */
+
+insert  into `wiki_pagedata_table`(`Page_ModID`,`PageData`,`PageEdit_User`,`Page_editDate`,`Page_Version`,`PageID`) values (1,'Hello','Test','6/9/2017','1',1),(2,'Hello Bye','Test','6/9/2017','2',1),(3,'this is two','Test','6/9/2017','3',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ MySQL_1.sql
@@ -0,0 +1,52 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.7.12-log : Database - liferaynew_1
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `istruzioni_per_la_compilazione` */
+
+DROP TABLE IF EXISTS `istruzioni_per_la_compilazione`;
+
+CREATE TABLE `istruzioni_per_la_compilazione` (
+  `Istruzioni_per_id` bigint(20) NOT NULL,
+  `purpose` varchar(2000) DEFAULT NULL,
+  `MainTitle` varchar(500) DEFAULT NULL,
+  `Tilte` varchar(1000) DEFAULT NULL,
+  `SubTitle` varchar(2000) DEFAULT NULL,
+  PRIMARY KEY (`Istruzioni_per_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `istruzioni_per_la_compilazione` */
+
+insert  into `istruzioni_per_la_compilazione`(`Istruzioni_per_id`,`purpose`,`MainTitle`,`Tilte`,`SubTitle`) values (1,'The purpose of home and hospital instruction is to provide instruction to a student with a temporary disability in the student’s home or in a hospital or other residential health facility, excluding state hospitals','Hospital Instruction Program','Purpose','Instructions');
+
+/*Table structure for table `istruzioni_per_la_compilazione_child` */
+
+DROP TABLE IF EXISTS `istruzioni_per_la_compilazione_child`;
+
+CREATE TABLE `istruzioni_per_la_compilazione_child` (
+  `Ins_id` bigint(20) NOT NULL,
+  `Instruction` varchar(4000) DEFAULT NULL,
+  `Istruzioni_per_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`Ins_id`),
+  KEY `FK_istruzioni_per_la_compilazione_child` (`Istruzioni_per_id`),
+  CONSTRAINT `FK_istruzioni_per_la_compilazione_child` FOREIGN KEY (`Istruzioni_per_id`) REFERENCES `istruzioni_per_la_compilazione` (`Istruzioni_per_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `istruzioni_per_la_compilazione_child` */
+
+insert  into `istruzioni_per_la_compilazione_child`(`Ins_id`,`Instruction`,`Istruzioni_per_id`) values (1,'School districts shall notify parents at the beginning of a school term of the availability of individualized instruction for pupils with a temporary disability. (EC sections 48206.3[d] and 48980)',1),(2,'A student with a temporary disability who is in a hospital or other residential health facility, excluding a state hospital, located outside of the school district in which the student’s parent or guardian resides, shall be deemed to have complied with the residency requirements for school attendance in the school district in which the hospital in located. (EC Section 48207)',1),(41277,'It is the primary responsibility of the parent or guardian of a student with a temporary disability to notify the school district in which the student is deemed to reside of the student’s presence in a qualifying hospital. Within five working days following notification from the parent or guardian, the school district shall determine whether the student will be able to receive individualized instruction, and, if the determination is positive, when the individualized instruction may begin. Individualized instruction shall start no later than five working days after the positive determination has been made. (EC Section 48208)',1),(41279,'EC Section 48206.3 specifies that for attendance accounting each clock hour of individualized instruction counts as one day of attendance. No student shall be credited with more than five days of attendance per calendar week or credited with more than the total number of calendar days that regular classes are offered by the district in any fiscal year.',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
